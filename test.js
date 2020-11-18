5ee52529cd7dfd9ad3da68df46b2d4742658e891
TwisterReiji <71079687+TwisterReiji@users.noreply.github.com>
2020-11-18 22:37:52 +0700

    bài tập buổi 1

---
+++ test.js
@@ -0,0 +1,11 @@
+function x() {
+    alert('OK')
+}
+function y() {
+    let a = document.getElementsByClassName('answer')
+    a[0].style.flexDirection = 'row-reverse';   
+}
+function z() {
+    let a = document.getElementsByClassName('answer')
+    a[0].style.flexDirection = 'row';
+}
